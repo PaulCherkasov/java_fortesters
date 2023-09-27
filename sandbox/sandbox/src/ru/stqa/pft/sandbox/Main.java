f27c5a1a8ca4e900e11ea7edc80e1e9cc6575bab
PaulCherkasov <warnov@yandex.ru>
2023-09-27 11:39:26 +0300

    functions added

--- sandbox/sandbox/src/ru/stqa/pft/sandbox/Main.java
+++ sandbox/sandbox/src/ru/stqa/pft/sandbox/Main.java
@@ -2,6 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        hello("world");
+
+        double len = 6;
+        System.out.println("Площадь равна " + len + " * " + len + " = " + area(len));
+
+        double a = 6;
+        double b = 8;
+        System.out.println("Reactangle area " + a + " * " + b + " = " + area(a,b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double l) {
+        return l * l;
+    }
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
