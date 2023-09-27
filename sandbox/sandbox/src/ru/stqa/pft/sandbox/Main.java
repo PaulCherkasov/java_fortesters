bc1e3a3aaa083d66e24e42361f132d325be027e1
PaulCherkasov <warnov@yandex.ru>
2023-09-27 12:46:35 +0300

    classes, objects and methods added

--- sandbox/sandbox/src/ru/stqa/pft/sandbox/Main.java
+++ sandbox/sandbox/src/ru/stqa/pft/sandbox/Main.java
@@ -4,20 +4,15 @@ public class Main {
     public static void main(String[] args) {
         hello("world");
 
-        double len = 6;
-        System.out.println("Площадь равна " + len + " * " + len + " = " + area(len));
+        Square s = new Square(6);
+        System.out.println("Площадь равна " + s.l + " * " + s.l + " = " + s.area());
 
-        double a = 6;
-        double b = 8;
-        System.out.println("Reactangle area " + a + " * " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle(6,9);
+        System.out.println("Rectangle area " + r.a + " * " + r.b + " = " + r.area());
     }
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-    public static double area(double l) {
-        return l * l;
-    }
-    public static double area(double a, double b) {
-        return a * b;
-    }
+
+
 }
